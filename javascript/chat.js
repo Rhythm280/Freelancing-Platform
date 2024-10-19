f8dd507d5fa3c57337daca2d5c63d68e82ddcda6
joy226 <grhythm240@gmail.com>
2024-10-19 13:44:56 +0530

    created message page

--- javascript/chat.js
+++ javascript/chat.js
@@ -37,33 +37,33 @@ function displayMessage(message, isSender = false) {
 }
 
 // client server conecton code
-const WebSocket = require('ws');
-const http = require('http');
-const express = require('express');
-const path = require('path');
+// const WebSocket = require('ws');
+// const http = require('http');
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+// const app = express();
+// const server = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
+// wss.on('connection', (ws) => {
+//     console.log('Client connected');
 
-    ws.on('message', (message) => {
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message.toString());
-            }
-        });
-    });
+//     ws.on('message', (message) => {
+//         wss.clients.forEach((client) => {
+//             if (client !== ws && client.readyState === WebSocket.OPEN) {
+//                 client.send(message.toString());
+//             }
+//         });
+//     });
 
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-});
+//     ws.on('close', () => {
+//         console.log('Client disconnected');
+//     });
+// });
 
-server.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
-});
+// server.listen(3000, () => {
+//     console.log('Server started on http://localhost:3000');
+// });
